6b42629370ea114bb3da2081e45d45d7c4a38e69
cgathergood <calumgathergood@gmail.com>
2018-08-05 11:52:22 +1000

    Inital commit, base example, needs Kotlin code review

---
+++ app/src/main/java/com/gathergood/calum/ardemo/MainActivity.kt
@@ -0,0 +1,164 @@
+package com.gathergood.calum.ardemo
+
+import android.graphics.Point
+import android.net.Uri
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import com.google.ar.core.Anchor
+import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
+import com.google.ar.core.TrackingState
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var arFragment: ArFragment
+
+    private var isTracking: Boolean = false
+    private var isHitting: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        arFragment = sceneform_fragment as ArFragment
+
+        // Adds a listener to the ARSceneView
+        // Called before processing each frame
+        arFragment.arSceneView.scene.setOnUpdateListener({ frameTime ->
+            arFragment.onUpdate(frameTime)
+            onUpdate()
+        })
+
+        // Set the onclick lister for our button
+        // Change this string to point to the .sfb file of your choice :)
+        floatingActionButton.setOnClickListener { addObject(Uri.parse("1227 Earth.sfb")) }
+        showFab(false)
+
+    }
+
+    // Simple function to show/hide our FAB
+    private fun showFab(enabled: Boolean) {
+        if (enabled) {
+            floatingActionButton.isEnabled = true
+            floatingActionButton.show()
+        } else {
+            floatingActionButton.isEnabled = false
+            floatingActionButton.hide()
+        }
+    }
+
+    // Updates the tracking state
+    private fun onUpdate() {
+        updateTracking()
+        // Check if the devices gaze is hitting a plane detected by ARCore
+        if (isTracking) {
+            val hitTestChanged = updateHitTest()
+            if (hitTestChanged) {
+                showFab(isHitting)
+            }
+        }
+
+    }
+
+    // Performs frame.HitTest and returns if a hit is detected
+    private fun updateHitTest(): Boolean {
+        val frame = arFragment.arSceneView.arFrame
+        val pt = getScreenCenter()
+        val hits: List<HitResult>
+        val wasHitting = isHitting
+        isHitting = false
+        if (frame != null) {
+            hits = frame.hitTest(pt.x.toFloat(), pt.y.toFloat())
+            for (hit in hits) {
+                val trackable = hit.trackable
+                if (trackable is Plane && trackable.isPoseInPolygon(hit.hitPose)) {
+                    isHitting = true
+                    break
+                }
+            }
+        }
+        return wasHitting != isHitting
+    }
+
+    // Makes use of ARCore's camera state and returns true if the tracking state has changed
+    private fun updateTracking(): Boolean {
+        val frame = arFragment.arSceneView.arFrame
+        val wasTracking = isTracking
+        isTracking = frame.camera.trackingState == TrackingState.TRACKING
+        return isTracking != wasTracking
+    }
+
+    // Simply returns the center of the screen
+    private fun getScreenCenter(): Point {
+        val vw = findViewById<View>(android.R.id.content)
+        return Point(vw.width / 2, vw.height / 2)
+    }
+
+    /**
+     * @param model The Uri of our 3D sfb file
+     *
+     * This method takes in our 3D model and performs a hit test to determine where to place it
+     */
+    private fun addObject(model: Uri) {
+        val frame = arFragment.arSceneView.arFrame
+        val point = getScreenCenter()
+        if (frame != null) {
+            val hits = frame.hitTest(point.x.toFloat(), point.y.toFloat())
+            for (hit in hits) {
+                val trackable = hit.trackable
+                if (trackable is Plane && trackable.isPoseInPolygon(hit.hitPose)) {
+                    placeObject(arFragment, hit.createAnchor(), model)
+                    break
+                }
+            }
+        }
+    }
+
+    /**
+     * @param fragment our fragment
+     * @param anchor ARCore anchor from the hit test
+     * @param model our 3D model of choice
+     *
+     * Uses the ARCore anchor from the hitTest result and builds the Sceneform nodes.
+     * It starts the asynchronous loading of the 3D model using the ModelRenderable builder.
+     */
+    private fun placeObject(fragment: ArFragment, anchor: Anchor, model: Uri) {
+        ModelRenderable.builder()
+                .setSource(fragment.context, model)
+                .build()
+                .thenAccept {
+                    addNodeToScene(fragment, anchor, it)
+                }
+                .exceptionally {
+                    Toast.makeText(this@MainActivity, "Error", Toast.LENGTH_SHORT).show()
+                    return@exceptionally null
+                }
+    }
+
+    /**
+     * @param fragment our fragment
+     * @param anchor ARCore anchor
+     * @param renderable our model created as a Sceneform Renderable
+     *
+     * This method builds two nodes and attaches them to our scene
+     * The Anchor nodes is positioned based on the pose of an ARCore Anchor. They stay positioned in the sample place relative to the real world.
+     * The Transformable node is our Model
+     * Once the nodes are connected we select the TransformableNode so it is available for interactions
+     */
+    private fun addNodeToScene(fragment: ArFragment, anchor: Anchor, renderable: ModelRenderable) {
+        val anchorNode = AnchorNode(anchor)
+        // TransformableNode means the user to move, scale and rotate the model
+        val transformableNode = TransformableNode(fragment.transformationSystem)
+        transformableNode.renderable = renderable
+        transformableNode.setParent(anchorNode)
+        fragment.arSceneView.scene.addChild(anchorNode)
+        transformableNode.select()
+    }
+}
